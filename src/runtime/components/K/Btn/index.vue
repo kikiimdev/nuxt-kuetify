915640a0dc36c155daeb25128a7bed402656258b
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-08-05 14:47:48 +0800

    feat: KBtn component

---
+++ src/runtime/components/K/Btn/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <VBtn>
+    <slot />
+
+    <VTooltip
+      v-if="tooltipText || tooltipProps"
+      activator="parent"
+      location="bottom"
+      v-bind="tooltipProps"
+    >
+      {{ tooltipText }}
+    </VTooltip>
+  </VBtn>
+</template>
+
+<script setup lang="ts">
+import type { VBtn, VTooltip } from "vuetify/components";
+
+defineProps<
+  VBtn["$props"] & {
+    hello?: string;
+    tooltipText?: string;
+    tooltipProps?: VTooltip["$props"];
+  }
+>();
+</script>
+
+<style></style>
